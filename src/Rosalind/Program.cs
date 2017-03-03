9e25959b63f8119ea58328599924e93a3e502a2b
= <=>
2017-03-03 23:13:07 +0000

    Some easy problems

---
+++ src/Rosalind/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Rosalind
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fileName = "/home/adam/Downloads/rosalind_inv.txt";
+            var answer = "";
+            using (var r = new StreamReader(new FileStream(fileName, FileMode.Open)))
+            {
+                r.ReadLine();
+                var a = r.ReadLine();
+                answer = string.Join(" ", CountInversions(
+                    a.Split(' ').Select(c => int.Parse(c)).ToArray()));
+            }
+            using (var w = new StreamWriter(new FileStream(fileName + "ans", FileMode.OpenOrCreate)))
+            {
+                w.Write(answer);
+                w.Flush();
+            }
+        }
+
+        public static int CountInversions(int[] array)
+        {
+            var inversions = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        inversions++;
+                    }
+                }
+            }
+            return inversions;
+        }
+
+        public static int[] MergeSort(int[] arrayToSort)
+        {
+            if (arrayToSort.Length == 1) return arrayToSort;
+            int halfWay = arrayToSort.Length / 2;
+            return MergeTwoSortedArrays(
+            MergeSort(arrayToSort.Take(halfWay).ToArray()),
+            MergeSort(arrayToSort.Skip(halfWay).ToArray()));
+
+        }
+
+        //O(n) run  and O(n) space
+        public static int[] MergeTwoSortedArrays(int[] arrayOne, int[] arrayTwo)
+        {
+            int[] answer = new int[arrayOne.Length + arrayTwo.Length];
+            var arrayOnePointer = 0;
+            var arrayTwoPointer = 0;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (arrayOnePointer < arrayOne.Length &&
+                (arrayTwoPointer >= arrayTwo.Length ||
+                arrayOne[arrayOnePointer] < arrayTwo[arrayTwoPointer]))
+                {
+                    answer[i] = arrayOne[arrayOnePointer];
+                    arrayOnePointer++;
+                }
+                else
+                {
+                    answer[i] = arrayTwo[arrayTwoPointer];
+                    arrayTwoPointer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ test/Rosalind.Tests/Tests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using Rosalind;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void MergeTwoSortedArrays()
+        {
+            Assert.Equal(new int[] { -5, 2, 4, 10, 11, 12, 18 },
+            Program.MergeTwoSortedArrays(
+                new int[] { 2, 4, 10, 18 },
+                new int[] { -5, 11, 12 }));
+        }
+
+        [Fact]
+        public void MergeSortWorks()
+        {
+            Assert.Equal(
+                new int[] { -20, -18, 1, 4, 4, 17, 19, 20, 20, 35 },
+                Program.MergeSort(new int[] { 20, 19, 35, -18, 17, -20, 20, 1, 4, 4 }));
+        }
+
+        [Fact]
+        public void CountingInversionsWorks()
+        {
+            Assert.Equal(2,
+                Program.CountInversions(new int[] { -6, 1, 15, 8, 10 }));
+        }
+    }
+}
