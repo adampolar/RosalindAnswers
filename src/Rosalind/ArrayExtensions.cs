c4e5b85fc7072c7bd98f7803364c5fb9fc46d9dd
= <=>
2017-03-04 11:54:51 +0000

    Refactored out of program class

---
+++ src/Rosalind/ArrayExtensions.cs
@@ -0,0 +1,126 @@
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Rosalind
+{
+    public static class ArrayExtensions
+    {
+        //On2 time 0n extra space
+        public static Tuple<int, int, int> Determine3Sum(this int[] array)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!dict.Keys.Contains(array[i]))
+                {
+                    dict.Add(array[i], i);
+                }
+            }
+            for (int j = 1; j < array.Length; j++)
+            {
+                for (int k = 0; k < j; k++)
+                {
+                    var relIndex = -1;
+                    dict.TryGetValue(-(array[j] + array[k]), out relIndex);
+                    //Try get value sets the out param back to 0, Cheers!!
+                    if (array[relIndex] + array[j] + array[k] != 0) relIndex = -1;
+
+                    if (relIndex != -1 && relIndex != j && relIndex != k)
+                    {
+                        var first = relIndex < k ? relIndex : k;
+                        var second = relIndex < k ? k : relIndex < j ? relIndex : j;
+                        var third = relIndex > j ? relIndex : j;
+                        return new Tuple<int, int, int>(first + 1, second + 1, third + 1);
+                    }
+                }
+            }
+            return null;
+        }
+
+        //On3 time no extra space
+        public static Tuple<int, int, int> Determine3Sum03(this int[] array)
+        {
+            for (int i = 2; i < array.Length; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    for (int k = 0; k < j; k++)
+                    {
+                        if (array[i] + array[j] + array[k] == 0)
+                        {
+                            return new Tuple<int, int, int>(k + 1, j + 1, i + 1);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static Tuple<int, int> Determine2Sum(this int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j] == -array[i])
+                    {
+                        return new Tuple<int, int>(j + 1, i + 1);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static int CountInversions(this int[] array)
+        {
+            var inversions = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        inversions++;
+                    }
+                }
+            }
+            return inversions;
+        }
+
+        public static int[] MergeSort(this int[] arrayToSort)
+        {
+            if (arrayToSort.Length == 1) return arrayToSort;
+            int halfWay = arrayToSort.Length / 2;
+            return MergeTwoSortedArrays(
+            MergeSort(arrayToSort.Take(halfWay).ToArray()),
+            MergeSort(arrayToSort.Skip(halfWay).ToArray()));
+
+        }
+
+        //O(n) run  and O(n) space
+        public static int[] MergeTwoSortedArrays(this int[] arrayOne, int[] arrayTwo)
+        {
+            int[] answer = new int[arrayOne.Length + arrayTwo.Length];
+            var arrayOnePointer = 0;
+            var arrayTwoPointer = 0;
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (arrayOnePointer < arrayOne.Length &&
+                (arrayTwoPointer >= arrayTwo.Length ||
+                arrayOne[arrayOnePointer] < arrayTwo[arrayTwoPointer]))
+                {
+                    answer[i] = arrayOne[arrayOnePointer];
+                    arrayOnePointer++;
+                }
+                else
+                {
+                    answer[i] = arrayTwo[arrayTwoPointer];
+                    arrayTwoPointer++;
+                }
+            }
+            return answer;
+        }
+
+    }
+}
--- test/Rosalind.Tests/Tests.cs
+++ test/Rosalind.Tests/Tests.cs
@@ -10,8 +10,7 @@ namespace Tests
         public void MergeTwoSortedArrays()
         {
             Assert.Equal(new int[] { -5, 2, 4, 10, 11, 12, 18 },
-            Program.MergeTwoSortedArrays(
-                new int[] { 2, 4, 10, 18 },
+            new int[] { 2, 4, 10, 18 }.MergeTwoSortedArrays(
                 new int[] { -5, 11, 12 }));
         }
 
@@ -20,30 +19,30 @@ namespace Tests
         {
             Assert.Equal(
                 new int[] { -20, -18, 1, 4, 4, 17, 19, 20, 20, 35 },
-                Program.MergeSort(new int[] { 20, 19, 35, -18, 17, -20, 20, 1, 4, 4 }));
+                new int[] { 20, 19, 35, -18, 17, -20, 20, 1, 4, 4 }.MergeSort());
         }
 
         [Fact]
         public void CountingInversionsWorks()
         {
             Assert.Equal(2,
-                Program.CountInversions(new int[] { -6, 1, 15, 8, 10 }));
+                new int[] { -6, 1, 15, 8, 10 }.CountInversions());
         }
         
         [Fact]
         public void TestTwoSum()
         {
-            Assert.Equal(null, Program.Determine2Sum(new int[] { 2, -3, 4, 10, 5 }));
+            Assert.Equal(null, new int[] { 2, -3, 4, 10, 5 }.Determine2Sum());
 
             var arr = new int[] { 8, 2, 4, -2, -8 };
-            var ans = Program.Determine2Sum(arr);
+            var ans = arr.Determine2Sum();
             Assert.NotNull(ans);
             Assert.Equal(arr[ans.Item1 - 1], -arr[ans.Item2 - 1]);
             
-            Assert.Equal(null, Program.Determine2Sum(new int[] { -5, 2, 3, 2, -4 }));
+            Assert.Equal(null, new int[] { -5, 2, 3, 2, -4 }.Determine2Sum());
 
             arr = new int[] { 5, 4, -5, 6, 8 };
-            ans = Program.Determine2Sum(arr);
+            ans = arr.Determine2Sum();
             Assert.NotNull(ans);
             Assert.Equal(arr[ans.Item1 - 1], -arr[ans.Item2 - 1]);
         }
@@ -51,21 +50,21 @@ namespace Tests
         [Fact]
         public void TestThreeSum()
         {
-            Assert.Equal(null, Program.Determine3Sum(new int[] { 2, -3, 4, 10, 5 }));
+            Assert.Equal(null, new int[] { 2, -3, 4, 10, 5 }.Determine3Sum());
 
             var arr = new int[] { 8, -6, 4, -2, -8};
-            var ans = Program.Determine3Sum(arr);
+            var ans = arr.Determine3Sum();
             Assert.NotNull(ans);
             Assert.Equal(0, arr[ans.Item1 - 1] + arr[ans.Item2 - 1] + arr[ans.Item3 - 1]);
 
             arr = new int[] { -5, 2, 3, 2, -4};
-            ans = Program.Determine3Sum(arr);
+            ans = arr.Determine3Sum();
             Assert.NotNull(ans);
             Assert.Equal(0, arr[ans.Item1 - 1] + arr[ans.Item2 - 1] + arr[ans.Item3 - 1]);
             
-            Assert.Equal(null, Program.Determine2Sum(new int[] { -5, 2, 3, 2, -4 }));
+            Assert.Equal(null, new int[] { -5, 2, 3, 2, -4 }.Determine2Sum());
 
-            Assert.Equal(null, Program.Determine3Sum(new int[] { 2, 4, -5, 6, 8 }));
+            Assert.Equal(null, new int[] { 2, 4, -5, 6, 8 }.Determine3Sum());
         }
     }
 }
