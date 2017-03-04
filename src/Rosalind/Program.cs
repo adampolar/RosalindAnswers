dfe43f52c9439ead70bc81ec7d12ffa1b90e597b
= <=>
2017-03-04 10:32:05 +0000

    Added 2Sum

--- src/Rosalind/Program.cs
+++ src/Rosalind/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Rosalind
 {
@@ -8,28 +9,50 @@ namespace Rosalind
     {
         public static void Main(string[] args)
         {
-            var fileName = "/home/adam/Downloads/rosalind_inv.txt";
-            var answer = "";
+            var fileName = "/home/adam/Downloads/rosalind_2sum.txt";
+            var answer = new List<string>();
             using (var r = new StreamReader(new FileStream(fileName, FileMode.Open)))
             {
                 r.ReadLine();
                 var a = r.ReadLine();
-                answer = string.Join(" ", CountInversions(
-                    a.Split(' ').Select(c => int.Parse(c)).ToArray()));
+                while (!String.IsNullOrEmpty(a))
+                {
+                    var ans = Determine2Sum(a.Split(' ').Select(c => int.Parse(c)).ToArray());
+                    answer.Add(ans != null ? (ans.Item1 + " " + ans.Item2) : (-1).ToString());
+                    a = r.ReadLine();
+                }
             }
             using (var w = new StreamWriter(new FileStream(fileName + "ans", FileMode.OpenOrCreate)))
             {
-                w.Write(answer);
+                foreach (var a in answer)
+                {
+                    w.WriteLine(a);
+                }
                 w.Flush();
             }
+        }
+
+        public static Tuple<int, int> Determine2Sum(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[j] == -array[i])
+                    {
+                        return new Tuple<int, int>(j + 1, i + 1);
+                    }
+                }
+            }
+            return null;
         }
 
         public static int CountInversions(int[] array)
         {
             var inversions = 0;
-            for(int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                for(int j = i + 1; j < array.Length; j++)
+                for (int j = i + 1; j < array.Length; j++)
                 {
                     if (array[j] < array[i])
                     {
--- test/Rosalind.Tests/Tests.cs
+++ test/Rosalind.Tests/Tests.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 using Rosalind;
+using System;
 
 namespace Tests
 {
@@ -27,6 +28,24 @@ namespace Tests
         {
             Assert.Equal(2,
                 Program.CountInversions(new int[] { -6, 1, 15, 8, 10 }));
+        }
+        
+        [Fact]
+        public void TestTwoSum()
+        {
+            Assert.Equal(null, Program.Determine2Sum(new int[] { 2, -3, 4, 10, 5 }));
+
+            var arr = new int[] { 8, 2, 4, -2, -8 };
+            var ans = Program.Determine2Sum(arr);
+            Assert.NotNull(ans);
+            Assert.Equal(arr[ans.Item1 - 1], -arr[ans.Item2 - 1]);
+            
+            Assert.Equal(null, Program.Determine2Sum(new int[] { -5, 2, 3, 2, -4 }));
+
+            arr = new int[] { 5, 4, -5, 6, 8 };
+            ans = Program.Determine2Sum(arr);
+            Assert.NotNull(ans);
+            Assert.Equal(arr[ans.Item1 - 1], -arr[ans.Item2 - 1]);
         }
     }
 }
